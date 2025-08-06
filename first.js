8e5c41d5183a559d0ce0d2f17d14be0de6552267
Niharikamalviya <neharikamalviya05@gmail.com>
2025-08-06 19:00:31 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,12 @@
+const product = {
+    tittle : "ball pen",
+    rating : 4,
+    offer : 5 ,
+    price : 270,
+
+
+};
+ console.log(product); 
+ //var = keyword
+ // let = keyword
+ // const = keyword
